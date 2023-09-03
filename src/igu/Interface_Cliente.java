ae7066f2f0c518945659988e1514321f107ed625
Jona <joni1107@gmail.com>
2023-09-03 18:46:11 -0300

    Union de los dos proyectos

--- src/igu/Interface_Cliente.java
+++ src/igu/Interface_Cliente.java
@@ -2,13 +2,56 @@ package igu;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import logica.Cliente;
 
 public class Interface_Cliente extends Component {
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextArea textArea1;
-    private JButton agregarButton;
+    private JTextField text_cedula;
+    private JTextField text_nombre;
+    private JTextArea text_vigente;
+    private JButton btn_ag;
     public JPanel panel2;
+
+    // Lista para almacenar clientes
+    public List<Cliente> listaClientes = new ArrayList<>();
+
+
+
+
+
+
+
+    public Interface_Cliente() {
+        // Agregar ActionListener al botón
+        btn_ag.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                agregarCliente();
+            }
+        });
+
+        listaClientes.add(new Cliente("19427769-5", "David Contardo", true));
+        listaClientes.add(new Cliente("17053692-4", "Jonathan Gutiérrez", true));
+        listaClientes.add(new Cliente("17741187-6", "Roberto Oreste", true));
+    }
+
+    private void agregarCliente() {
+        String cedula = text_cedula.getText();
+        String nombre = text_nombre.getText();
+
+        Cliente cliente = new Cliente(cedula, nombre, true);
+
+        // Validar cédula y nombre antes de agregar
+        if (cliente.validarCedula(cedula) && cliente.validarNombre(nombre)) {
+            listaClientes.add(cliente);
+            text_vigente = text_vigente;
+        }
+        /*listaClientes.add(cliente);
+        text_vigente.append(nombre + " ha sido agregado y está vigente.\n");*/
+    }
 
 
     public void mostrarMensajeInfo(String mensaje) {
@@ -20,4 +63,7 @@ public class Interface_Cliente extends Component {
         JOptionPane.showMessageDialog(this, mensaje, "Error", JOptionPane.ERROR_MESSAGE);
     }
 
+    public List<Cliente> getListaClientes() {
+        return listaClientes;
+    }
 }
